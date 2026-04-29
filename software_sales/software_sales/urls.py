e2e75a7aab0fa944262862868fac9d302c559b05
aapolo7 <orlandoconceicao94@gmail.com>
2026-04-29 14:54:33 -0400

    corrige Swagger e Redoc em branco adicionando schema JSON e ajustes de produção

--- software_sales/software_sales/urls.py
+++ software_sales/software_sales/urls.py
@@ -19,6 +19,11 @@ schema_view = get_schema_view(
     permission_classes=(permissions.AllowAny,),
 )
 
+path(
+    "swagger.json",
+    schema_view.without_ui(cache_timeout=0),
+    name="schema-json"
+),
 
 def home(request):
     return JsonResponse({

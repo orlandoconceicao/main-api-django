3e8237f7317fdc73568c1895a74bdb1ab1cecfb7
aapolo7 <orlandoconceicao94@gmail.com>
2026-05-02 12:38:19 -0400

    fix: ajusta imports e configuração de apps (software_sales.core / software_sales.courses)

--- software_sales/courses/apps.py
+++ software_sales/courses/apps.py
@@ -2,7 +2,7 @@ from django.apps import AppConfig
 
 class CoursesConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'courses'
+    name = 'software_sales.courses'
 
     def ready(self):
         import software_sales.courses.signals

3b261840a7bc5c38bc08f9fc87af119f6cb33eca
aapolo7 <orlandoconceicao94@gmail.com>
2026-05-02 12:49:02 -0400

    fix: corrige AppConfig e caminhos dos apps core e courses

--- software_sales/courses/apps.py
+++ software_sales/courses/apps.py
@@ -2,7 +2,7 @@ from django.apps import AppConfig
 
 class CoursesConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'software_sales.courses'
+    name = 'software_sales.core'
 
     def ready(self):
         import software_sales.courses.signals
